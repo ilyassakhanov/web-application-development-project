49886d96809bd640f6108130bd1d51cb2276b16a
ilyassakhanov <ilyas.sakhanov@gmail.com>
2021-05-06 13:31:51 +0200

    modify note, add routes outline

--- models/note.js
+++ models/note.js
@@ -4,11 +4,18 @@ const noteSchema = new mongoose.Schema({
 
   username: {
     type: String,
+    required: true
   }, 
   password: {
+    type: String,
+    required: true
+  },
+  title : {
     type: String
   },
-  
+  description: {
+    type: String
+  } // add categories field
   
 });
   
--- routes.js
+++ routes.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const Note = require('./models/note');
+// remember pagination
 
-router.get('/', async (req, res) => {
-    const result = await Note();
-    
-})
+router.get('/test', (req, res) => {
+    // const result = await Note();
+    res.json({ test: 'this is a test route' });
+});
+
+
+router.get('/', (req, res) => {
+    res.json({ test: 'latest change'});
+});
+
+router.get('/jobs', (req, res) => {
+    res.json({ test: 'all jobs avaliable'});
+});
+
+router.post('/create', (req, res) => {
+    res.json({ test: 'creates a new job'});
+});
+
+
+
+module.exports = router;
