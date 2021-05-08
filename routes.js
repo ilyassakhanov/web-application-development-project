f4fabf8861642846f44f1f1cd364a591f347dc18
ilyassakhanov <ilyas.sakhanov@gmail.com>
2021-05-08 13:21:52 +0200

    add basic structure for register api

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Note = require('./models/note');
+const Job = require('./models/job');
 // remember pagination
 
 router.get('/test', (req, res) => {
@@ -10,15 +10,28 @@ router.get('/test', (req, res) => {
 
 
 router.get('/', (req, res) => {
-    res.json({ test: 'latest change'});
+    res.json({ test: 'latest change' });
 });
 
 router.get('/jobs', (req, res) => {
-    res.json({ test: 'all jobs avaliable'});
+    res.json({ test: 'all jobs avaliable' });
 });
 
 router.post('/create', (req, res) => {
-    res.json({ test: 'creates a new job'});
+    res.json({ test: 'creates a new job' });
+});
+
+router.post('/login', (req, res) => {
+    res.json({ test: 'used for a login' });
+});
+
+router.post('/register', async (req, res) => {
+    if ((req.body.username != null || req.body.username != "") && (req.body.password != null || req.body.password != "")) {
+        const job = new Job({
+            username: req.body.username,
+            password: req.body.password
+        });
+    }
 });
 
 
