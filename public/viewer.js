f2b5fb83c52edefaa9e8cca29810342f4bc7a1cc
ilyassakhanov <ilyas.sakhanov@gmail.com>
2021-05-26 20:55:08 +0200

    Change syntax

--- public/viewer.js
+++ public/viewer.js
@@ -1,15 +1,15 @@
 Vue.component('job-item', {
     props: ['job'],
-    template: '<li>{{ job.title }}</li>'
+    template: '<ul>{{ job.title }} ,{{ job.description }}</ul>'
 })
 
 var app = new Vue({
     el: '#viewer',
     data: {
       offers: [
-        { id: 0, title: 'Title1' },
-        { id: 1, title: 'Title2' },
-        { id: 2, title: 'Title3' }
+        {title: 'Title1', description: 'lorum ipsum'},
+        {title: 'Title2', description: 'lorum ipsum'},
+        {title: 'Title3', description: 'lorum ipsum'}
       ]
     }
 })
